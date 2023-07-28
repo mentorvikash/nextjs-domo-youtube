36aa64628cc1150d5233e0a29b0d592e63511e72
mentorvikash <mentorvikash@gmail.com>
2023-07-28 23:41:23 +0530

    initial commit

---
+++ pages/blog/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Blog() {
+  return <div>Blog Pages</div>;
+}
+
+export default Blog;
---
+++ pages/blog/second.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Second() {
+  return <div>Second blog</div>;
+}
+
+export default Second;
